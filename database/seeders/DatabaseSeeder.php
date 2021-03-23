ea95c3d63e743cc972239c7b7ef0899a17b83c0f
Hisham <hishamzeyad98@gmail.com>
2021-03-23 18:01:37 +0200

    add db login

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,36 +1,24 @@
 <?php
-
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
-use  App\Models\User;
-use App\Models\Category;
-use  App\Models\Post;
-use App\Models\Comment;
+
 class DatabaseSeeder extends Seeder
 {
-    /**
+    /**  
      * Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(50)->create();
-        // \App\Models\Category::factory(15)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
 
-        User::factory()->count(50)->create();
-         Category::factory()->count(50)->create();
-         Post::factory()->count(50)->create();
-         Comment::factory()->count(50)->create();
-        // \App\Models\Post::factory(500)->create();
-        // \App\Models\Comment::factory(1500)->create();
+        DB::table('users')->truncate();
 
-                // $this->call(UsersTableSeeder::class);
+        $this->call([ UsersTableSeeder::class]);
 
-                // factory( \App\User::class , 50 )->create();
-                // factory( \App\Category::class , 15 )->create();
-                // factory( \App\Post::class , 500 )->create();
-                // factory( \App\Comment::class , 1500 )->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
     }
 }
